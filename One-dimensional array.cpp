e0518b0e2b2f0baea190df9a55d90f64795a942e
Mark <160011996+GertMark2@users.noreply.github.com>
2025-01-21 08:47:01 +0300

    Add files via upload

---
+++ One-dimensional array.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	float numbers[5];
+	for (int i = 0; i < 5; i++) {
+		cout << "Enter number " << i<< ":";
+		cin >> numbers[i];
+	}
+	
+	float summa = 0;
+	float min = numbers[0];
+	for (int i = 0; i < 5; i++) {
+		summa += numbers[i];
+		if (numbers[i] < min)
+			min = numbers[i];
+	}
+	cout << " Summa:" << summa << endl;
+	cout << " Min:" << min << endl;
+
+	for (int i = 0; i < 5; i++)
+		cout << "El:" << numbers[i] << endl;
+	return 0;
+}
